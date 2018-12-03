1d2477f0b60de128580632ce5b132e0b2514e450
mgella <Meen@098>
2018-12-03 00:45:32 -0800

    Update index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,7 @@
-$(document).ready(function () {
+\$(document).ready(function () {
   var timeData = [],
     temperatureData = [],
-    smokeData = [],
+smokeData = [],
     humidityData = [];
   var data = {
     labels: timeData,
@@ -31,25 +31,6 @@ $(document).ready(function () {
     ]
   }
 
-
-  var data2 = {
-    labels: timeData,
-    datasets: [
-      {
-        fill: false,
-        label: 'SmokeRate',
-        yAxisID: 'SmokeRate',
-        borderColor: "rgba(255, 204, 0, 1)",
-        pointBoarderColor: "rgba(255, 204, 0, 1)",
-        backgroundColor: "rgba(255, 204, 0, 0.4)",
-        pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
-        pointHoverBorderColor: "rgba(255, 204, 0, 1)",
-        data: smokeData
-      }
-    ]
-  }
-
-
   var basicOption = {
     title: {
       display: true,
@@ -77,41 +58,14 @@ $(document).ready(function () {
     }
   }
 
-  var basicOption2 = {
-                  title: {
-                  display: true,
-                  text: 'Smoke Real-time Data',
-                  fontSize: 36
-                  },
-                  scales: {
-                  yAxes: [{
-                          id: 'SmokeRate',
-                          type: 'linear',
-                          scaleLabel: {
-                          labelString: 'SmokeRate',
-                          display: true
-                          },
-                          position: 'left',
-                          }]
-                  }
-                  }
-
-
   //Get the context of the canvas element we want to select
   var ctx = document.getElementById("myChart").getContext("2d");
-  var ctx2 = document.getElementById("myChart2").getContext("2d");
   var optionsNoAnimation = { animation: false }
   var myLineChart = new Chart(ctx, {
     type: 'line',
     data: data,
     options: basicOption
   });
-
-    var myLineChart2 = new Chart(ctx2, {
-                                      type: 'line',
-                                      data: data2,
-                                      options: basicOption2
-                                              });
 
   var ws = new WebSocket('wss://' + location.host);
   ws.onopen = function () {
@@ -126,7 +80,6 @@ $(document).ready(function () {
       }
       timeData.push(obj.time);
       temperatureData.push(obj.temperature);
-      smokeData.push(obj.smokeRate);
       // only keep no more than 50 points in the line chart
       const maxLen = 50;
       var len = timeData.length;
@@ -142,16 +95,7 @@ $(document).ready(function () {
         humidityData.shift();
       }
 
-
- if (obj.smokeRate) {
-        smokeData.push(obj.smokeRate);
-      }
-      if (smokeData.length > maxLen) {
-        smokeData.shift();
-      }
-
       myLineChart.update();
-      myLineChart2.update();
     } catch (err) {
       console.error(err);
     }

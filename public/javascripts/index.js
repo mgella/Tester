3eea3ac659d30e18d80e9815645b170ce6bacb20
mgella <Meen@098>
2018-12-03 00:34:52 -0800

    h

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   var timeData = [],
     temperatureData = [],
+    smokeData = [],
     humidityData = [];
   var data = {
     labels: timeData,
@@ -30,6 +31,25 @@ $(document).ready(function () {
     ]
   }
 
+
+  var data2 = {
+    labels: timeData,
+    datasets: [
+      {
+        fill: false,
+        label: 'Smoke',
+        yAxisID: 'Smoke',
+        borderColor: "rgba(255, 204, 0, 1)",
+        pointBoarderColor: "rgba(255, 204, 0, 1)",
+        backgroundColor: "rgba(255, 204, 0, 0.4)",
+        pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
+        pointHoverBorderColor: "rgba(255, 204, 0, 1)",
+        data: smokeData
+      }
+    ]
+  }
+
+
   var basicOption = {
     title: {
       display: true,
@@ -57,14 +77,41 @@ $(document).ready(function () {
     }
   }
 
+  var basicOption2 = {
+                  title: {
+                  display: true,
+                  text: 'Smoke Real-time Data',
+                  fontSize: 36
+                  },
+                  scales: {
+                  yAxes: [{
+                          id: 'Smoke',
+                          type: 'linear',
+                          scaleLabel: {
+                          labelString: 'Smoke',
+                          display: true
+                          },
+                          position: 'left',
+                          }]
+                  }
+                  }
+
+
   //Get the context of the canvas element we want to select
   var ctx = document.getElementById("myChart").getContext("2d");
+  var ctx2 = document.getElementById("myChart2").getContext("2d");
   var optionsNoAnimation = { animation: false }
   var myLineChart = new Chart(ctx, {
     type: 'line',
     data: data,
     options: basicOption
   });
+
+    var myLineChart2 = new Chart(ctx2, {
+                                              type: 'line',
+                                              data: data2,
+                                              options: basicOption2
+                                              });
 
   var ws = new WebSocket('wss://' + location.host);
   ws.onopen = function () {
@@ -74,7 +121,7 @@ $(document).ready(function () {
     console.log('receive message' + message.data);
     try {
       var obj = JSON.parse(message.data);
-      if(!obj.time || !obj.temperature) {
+      if(!obj.time || !obj.temperature || !obj.smokeRate) {
         return;
       }
       timeData.push(obj.time);
@@ -94,7 +141,16 @@ $(document).ready(function () {
         humidityData.shift();
       }
 
+
+ if (obj.smokeRate) {
+        smokeData.push(obj.smokeRate);
+      }
+      if (smokeData.length > maxLen) {
+        smokeData.shift();
+      }
+
       myLineChart.update();
+      myLineChart2.update();
     } catch (err) {
       console.error(err);
     }
